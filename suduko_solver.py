13808ddb1b87c3a45f1249020538d7c1d8b3cced
Saurabey ?? <saurabbaral03@gmail.com>
2023-05-19 10:56:49 +0545

    3

--- suduko_solver.py
+++ suduko_solver.py
@@ -1,45 +1,50 @@
+import math
+from grid import grid
 def is_valid_move(grid, row, col, num):
     # Check row and column
-    for i in range(9):
+    for i in range(len(grid)):
         if grid[row][i] == num or grid[i][col] == num:
             return False
-    
+
     # Check the 3x3 grid
-    small_grid_row = (row // 3) * 3
-    small_grid_col = (col // 3) * 3
-    for i in range(3):
-        for j in range(3):
+    x = int(math.sqrt(len(grid)))
+    small_grid_row = (row // x) * x
+    small_grid_col = (col // x) * x
+    for i in range(x):
+        for j in range(x):
             if grid[small_grid_row + i][small_grid_col + j] == num:
                 return False
-    
+
     return True
 
 def solve_sudoku(grid):
-    for row in range(9):
-        for col in range(9):
+    for row in range(len(grid)):
+        for col in range(len(grid)):
             if grid[row][col] == 0:
-                for num in range(1, 10):
+                for num in range(1, len(grid) + 1):
                     if is_valid_move(grid, row, col, num):
                         grid[row][col] = num
                         if solve_sudoku(grid):
                             return True
-                        grid[row][col] = 0  # Undo the move
+                        else:
+                            grid[row][col] = 0
                 return False
     return True
 
-grid = [
-    [0, 0, 0, 0, 0, 0, 6, 8, 0],
-    [0, 0, 0, 0, 7, 3, 0, 0, 9],
-    [3, 0, 9, 0, 0, 0, 0, 4, 5],
-    [4, 9, 0, 0, 0, 0, 0, 0, 0],
-    [8, 0, 3, 0, 5, 0, 9, 0, 2],
-    [0, 0, 0, 0, 0, 0, 0, 3, 6],
-    [9, 6, 0, 0, 0, 0, 3, 0, 8],
-    [7, 0, 0, 6, 8, 0, 0, 0, 0],
-    [0, 2, 8, 0, 0, 0, 0, 0, 0]
+grid = [    [5, 3, 4, 0, 7, 8, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
-solve_sudoku(grid)
 
-for row in grid:
-    print(row)
+if solve_sudoku(grid):
+    for row in grid:
+        print(row)
+else:
+    print("Cannot be solved")
